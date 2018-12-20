8db0716f76f8fee3839ef717ae359deafff1dbcd
Luis Betancourt <lbetancourt@usnews.com>
2018-12-19 21:49:51 -0500

    Remove prebid

--- src/components/Provider/index.js
+++ src/components/Provider/index.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PubSub from '../../lib/Pubsub';
-import '../../../prebid'; // window.pbjs
 
 import {
   getVersion,
