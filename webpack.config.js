2ebce1f7fb6cca0a793d46fbbd9befd512bcb3f9
Luis Betancourt <webdeveloperpr@gmail.com>
2018-12-26 08:42:15 -0500

    Remove extraconfig

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,7 @@ const config = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: { 
-          loader: 'babel-loader',            
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: ["@babel/plugin-proposal-class-properties"]
-          }
+          loader: 'babel-loader'
         },
       }
     ]
