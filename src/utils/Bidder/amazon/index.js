f10d5be868518105fe120514eaab48d5e43111f9
Luis Betancourt <lbetancourt@usnews.com>
2019-01-04 02:29:17 -0500

    Add amazon bidder implementation

--- src/utils/Bidder/amazon/index.js
+++ src/utils/Bidder/amazon/index.js
@@ -1,23 +1,31 @@
-import Bidder from '../';
-// import loadScript from './initialize';
+/* eslint-disable no-console */
+import { Bidder } from 'react-ads';
+import loadScript from './initialize';
 
 const bidder = new Bidder('amazon');
 
 bidder.init = () => {
-  // loadScript();
-  // window.apstag.init({
-  //   pubID: 3143,
-  //   adServer: 'googletag',
-  //   bidTimeout: 1000,
-  // });
+  loadScript();
+  window.apstag.init({
+    pubID: 123,
+    adServer: 'googletag',
+    bidTimeout: 1000,
+  });
   console.log('amazon initialized');
 };
 
 bidder.fetchBids = adUnits => new Promise((resolve, reject) => {
-  setTimeout(reject, bidder.safeTimeout);
-  if (bidder.isReady === false) reject({});
-  console.log('amazon:fetchBids');
-  // window.apstag.fetchBids({ slots }, resolve);
+  const id = setTimeout(reject, bidder.safeTimeout);
+
+  const bids = adUnits.reduce((acc, { bids }) => {
+    const newAcc = acc.concat(bids);
+    return newAcc;
+  }, []);
+
+  window.apstag.fetchBids({ slots: bids }, (...props) => {
+    clearTimeout(id);
+    resolve(...props);
+  });
 });
 
 bidder.onBidWon = () => {
@@ -29,7 +37,21 @@ bidder.onTimeout = () => {
 };
 
 bidder.handleResponse = (bids) => {
-  console.log('amazon:response');
+  const googletag = window.googletag;
+  googletag.cmd.push(function () {
+    console.log('amazon bids', bids);
+    bids.forEach(({ amzniid, amznbid, slotID }) => {
+      window.googletag
+        .pubads()
+        .getSlots().forEach(slot => {
+          const id = slot.getSlotElementId();
+          if (id === slotID) {
+            slot.setTargeting('amzniid', amzniid);
+            slot.setTargeting('amznbid', amznbid);
+          }
+        });
+    });
+  });
 };
 
 export default bidder;
