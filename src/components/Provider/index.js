7c72f1d17e378f308606d5997582f93265742bf8
Luis Betancourt <webdeveloperpr@gmail.com>
2019-01-26 00:34:41 -0500

    Use CDN links

--- src/components/Provider/index.js
+++ src/components/Provider/index.js
@@ -126,9 +126,9 @@ class Provider extends Component {
 
       const timeout = setTimeout(reject, 4000);
       const stylesheets = [
-        '//googleads.github.io/videojs-ima/node_modules/video.js/dist/video-js.min.css',
-        '//googleads.github.io/videojs-ima/node_modules/videojs-contrib-ads/dist/videojs.ads.css',
-        '//googleads.github.io/videojs-ima/dist/videojs.ima.css'
+        'https://cdnjs.cloudflare.com/ajax/libs/video.js/7.5.0/video-js.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/6.6.1/videojs-contrib-ads.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/videojs-ima/1.5.2/videojs.ima.min.css'
       ];
 
       let remaining = stylesheets.length;
@@ -173,11 +173,11 @@ class Provider extends Component {
       this.videoStatus = STARTED;
     }
 
-    return this.loadVideoScripts(['//googleads.github.io/videojs-ima/node_modules/video.js/dist/video.min.js'], '-1')
+    return this.loadVideoScripts(['https://cdnjs.cloudflare.com/ajax/libs/video.js/7.5.0/video.min.js'], '-1')
       .then(() => this.loadVideoScripts([
         '//imasdk.googleapis.com/js/sdkloader/ima3.js',
-        '//googleads.github.io/videojs-ima/node_modules/videojs-contrib-ads/dist/videojs.ads.min.js',
-        '//googleads.github.io/videojs-ima/dist/videojs.ima.js'
+        'https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/6.6.1/videojs-contrib-ads.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/videojs-ima/1.5.2/videojs.ima.min.js'
       ]), '-2')
       .then(() => this.loadVideoCss())
       .then(() => {
